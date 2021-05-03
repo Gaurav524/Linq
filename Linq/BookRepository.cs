328ef84a09b71a5fc7fd40f4e5fa3de2e7ee83ea
Gaurav Gupta <GGUPTA@volvocars.com>
2021-05-03 14:48:44 +0200

    Linq functions.

---
+++ Linq/BookRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    public class BookRepository
+    {
+        public IEnumerable<Book> GetBook()
+        {
+            return new List<Book>
+            {
+              new Book(){Title="Ado", Price=5},
+              new Book(){Title="Mvc", Price=6},
+              new Book(){Title="Csharp", Price=7},
+              new Book(){Title="Asp.Net", Price=11},
+              new Book(){Title="Cloud", Price=16},
+            };
+        }
+    }
+}
---
+++ Linq/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var books = new BookRepository().GetBook();
+            var book = books.Single(b => b.Title == "Ado345");
+            Console.WriteLine(book.Title);
+            //LinqBasicMethods(books);
+
+            //books.Where(b => b.Price < 10);
+            //books.Single(b => b.Price < 10);
+            //books.SingleOrDefault(b => b.Price < 10);
+            //books.Count();
+            //books.Sum(b=>b.Price);
+            //books.First(b => b.Price < 10);
+            //books.FirstOrDefault(b => b.Price < 10);
+            //books.Max();
+            //books.Min();
+            //books.Average(b => b.Price);
+            //books.Skip(2).Take(3);
+            //books.Last();
+            //books.LastOrDefault();
+
+        }
+
+        private static void LinqBasicMethods(IEnumerable<Book> books)
+        {
+            //LINQ Query Operators
+            var cheaperBooks =
+                from b in books
+                where b.Price < 10
+                orderby b.Title
+                select b;
+
+            foreach (var cheapBook in cheaperBooks)
+                Console.WriteLine(cheapBook.Title + " " + cheapBook.Price);
+
+            //LINQ Extension Methods
+            var cheapBooks = books
+                .Where(b => b.Price < 10)
+                .OrderBy(b => b.Title)
+                .Select(b => b.Title);
+
+            foreach (var cheapBook in cheapBooks)
+                Console.WriteLine(cheapBook);
+        }
+    }
+}
